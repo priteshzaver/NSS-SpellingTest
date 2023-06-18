6dd4ee876f130d1f178b18984b26f449bcc7b16c
Pritesh Zaver <priteshzaver@gmail.com>
2023-06-18 16:15:41 -0500

    part 1 complete

---
+++ src/components/SpeechUtterance.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps(['word'])
+
+const sayWord = () => {
+  const utterance = new SpeechSynthesisUtterance(props.word)
+  return speechSynthesis.speak(utterance)
+}
+</script>
+
+<template>
+  <button @click="sayWord()">Say Word</button>
+</template>
+
+<style scoped></style>
